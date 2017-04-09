745b400e69ddb06965f33a5de211144518b50516
Christophe Lee <christophelee2004@icloud.com>
2017-04-09 17:33:41 +0800

    nothing much

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Color Print Module #
 ### colorprint.colorprint(Text, Text Color, Background Color, Options) ###
 
+>A Python module that allows you to print in color
 - colors: black, red, green, yellow, blue, magenta, cyan, light gray, dark gray, light red, light green, light yellow, light blue, light magenta,light cyan, white
 - options: bold, dim, underline, blink, reverse, hidden
 
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'colorprint',
+    packages = ['colorprint'], 
+    version = '1.0',
+    description = 'A Python module that allows you to print in color',
+    author = 'Christophe Lee',
+    author_email = 'christophelee2004@icloud.com',
+    url = 'https://github.com/Myrmechrislee/ColorPrint.git', 
+    download_url = 'https://github.com/Myrmechrislee/ColorPrint/archive/1.0.tar.gz', # I'll explain this in a second
+    keywords = ["print", "color", "bash", "echo"], 
+    classifiers = [],
+)
